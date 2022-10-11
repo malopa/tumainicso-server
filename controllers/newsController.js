c22ab7ed5fa4b48dcc70950c88f365df4238554b
quicker <malopalusajo@gmail.com>
2022-10-11 16:40:34 +0300

    pdf

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -10,7 +10,7 @@ module.exports = {
 
         try{
             let files = req.files;
-
+            console.log(files)
             const s3 = new AWS.S3({
                 accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
                 secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
